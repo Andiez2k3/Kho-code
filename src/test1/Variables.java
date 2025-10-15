5640555fb324b71b92e71ee01631e51e647e2304
Andiez2k3 <quyen0946325154@gmail.com>
2025-10-15 21:20:10 +0700

    add lock

--- src/test1/Variables.java
+++ src/test1/Variables.java
@@ -5,7 +5,18 @@ public class Variables {
 		int a = 100;
 		int b = 50;
 		int c = 99;
+<<<<<<< HEAD
 	 System.out.println( a + b - c);
 	}
 
+=======
+		System.out.println(a + b - c);
+		int tong = a + b - c;
+		for (int i = 0; i < 10; i++) {
+			int giamdan = tong - 1;
+			System.out.println(giamdan);
+			tong = giamdan;
+		}
+	}
+>>>>>>> 82ebe30 (1)
 }
