44d18a43d77d6dab1a4ee45df258c5e1711f418a
Andiez2k3 <quyen0946325154@gmail.com>
2025-10-31 08:26:07 +0700

    Mang array

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -1,13 +1,27 @@
 package test1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test1 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.println(a + " and " + b);
+		System.out.println("Nhap so mang array:");
+		int size = sc.nextInt();
+		int[] A = new int[size];
+		for (int i = 0; i < size; i++) {
+			System.out.println("Phan tu la :" + A[i]);
+			A[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(A));
+		int Max = A[0];
+		int Min = A[0];
+		for (int j = 0; j < size; j++) {
+			Max = Math.max(Max, A[j]);
+			Min = Math.min(Min, A[j]);
+		}
+		System.out.println("Max :" + Max);
+		System.out.println("Min :" + Min);
 	}
 }
